b3219bee0ab33636473259336c7574e96dc033e1
Glacy <glacy.kelly3@gmail.com>
2022-03-27 22:13:46 -0300

    adicionando código

--- day-2-7-days-of-code/index.js
+++ day-2-7-days-of-code/index.js
@@ -1,24 +1,15 @@
-let numeroUm = 1;
-let stringUm = '1';
-let numeroTrinta = 30;
-let stringTrinta = '30';
-let numeroDez = 10;
-let stringDez = '10';
+const nome =  prompt("Qual é o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagemDeProgramacao = prompt("Qual linguagem de programação você está estudando?");
 
-if (numeroUm == stringUm) {
-    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
-} else {
-    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagemDeProgramacao}!`);
+
+const resposta = prompt(`Você gosta de estudar ${linguagemDeProgramacao}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+if(resposta === '1'){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}else{
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
 }
-  
-if (numeroTrinta === stringTrinta) {
-    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
-} else {
-    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
-}
-  
-if (numeroDez == stringDez) {
-    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
-} else {
-    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
-}   
+
