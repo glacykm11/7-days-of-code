b5a5706ef5b2a4d6f315c1575c1283ced14da175
Glacy <glacy.kelly3@gmail.com>
2022-04-02 17:36:01 -0300

    dia 4

---
+++ day-4-7-days-of-code/index.js
@@ -0,0 +1,22 @@
+function gerarNumeroAleatrio(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+//let numeroAleatorio = gerarNumeroAleatrio(0, 10);
+let numeroAleatorio = 2;
+let count = 3;
+
+let chute = prompt("Qual valor você deseja chutar?");
+
+if(chute == numeroAleatorio){
+    alert(`Parabéns, você acertou!`);
+}
+
+while(count != 1){
+    count--;
+    prompt(`Qual o valor você deseja chutar? Você ainda tem mais ${count} chances`);
+}
+
+if(count == 1){
+    alert(`Poxa, suas chances acabaram! O número era ${numeroAleatorio}!`);
+}
