2087c38bb135d86387650a2afbc9e92657e8e1a2
Glacy <glacy.kelly3@gmail.com>
2022-04-02 23:33:00 -0300

    dia 5

---
+++ day-5-7-days-of-code/index.js
@@ -0,0 +1,38 @@
+let comida;
+let categoria;
+let resposta;
+let arrayFrutas = [];
+let arrayLaticinios = [];
+let arrayCongelados = [];
+let arrayDoces = [];
+
+resposta = prompt("Deseja adicionar uma comida na sua lista de compras?");
+
+while (resposta == 'sim'){
+   resposta = prompt("Deseja adicionar uma comida na sua lista de compras?");
+   comida = prompt("Qual comida você deseja inserir?");
+   categoria = prompt("Em qual categoria essa comida se encaixa? Frutas, laticínios, congelados ou doces?");
+   
+   switch(categoria){
+    case 'frutas': 
+        arrayFrutas.push(comida);
+        break;
+    case 'laticínios':
+        arrayLaticinios.push(comida);
+        break;
+    case 'congelados':
+        arrayCongelados.push(comida);
+        break;
+    case 'doces':
+        arrayDoces.push(comida);
+        break;
+   }
+}
+
+alert(`Lista de supermercado: 
+    Frutas: ${arrayFrutas},
+    Laticínios: ${arrayLaticinios},
+    Congelados: ${arrayCongelados},
+    Doces: ${arrayDoces}
+`);
+
