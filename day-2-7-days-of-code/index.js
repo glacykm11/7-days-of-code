314c5d138f4006c3d1bdb6b41794ad827ca5d90e
Glacy <glacy.kelly3@gmail.com>
2022-04-30 12:46:31 -0300

    refatorando html e js

--- day-2-7-days-of-code/index.js
+++ day-2-7-days-of-code/index.js
@@ -1,15 +1,37 @@
-const nome =  prompt("Qual é o seu nome?");
-const idade = prompt("Quantos anos você tem?");
-const linguagemDeProgramacao = prompt("Qual linguagem de programação você está estudando?");
+// const nome =  prompt("Qual é o seu nome?");
+// const idade = prompt("Quantos anos você tem?");
+// const linguagemDeProgramacao = prompt("Qual linguagem de programação você está estudando?");
+
+let nomeForm = document.getElementById('nome-form');
+let idadeForm = document.getElementById('idade-form');
+let linguagemProgramacaoForm = document.getElementById('linguagem-programacao-form');
+// let botaoTeste = ;
+
+document.getElementById('teste-botao').addEventListener("click", () => {
+    if(nomeForm.value.length && idadeForm.value.length && linguagemProgramacaoForm.value.length == 0){
+        console.log(`Olá ${nomeForm.value}, você tem ${idadeForm.value} anos e já está aprendendo ${linguagemProgramacaoForm.value}!`)
+    
+        alert(`Olá ${nomeForm.value}, você tem ${idadeForm.value} anos e já está aprendendo ${linguagemProgramacaoForm.value}!`);
+    }
+})
+
+// function validaForms(){
+    
+// }
 
 
-alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagemDeProgramacao}!`);
 
-const resposta = prompt(`Você gosta de estudar ${linguagemDeProgramacao}? Responda com o número 1 para SIM ou 2 para NÃO.`);
 
-if(resposta === '1'){
-    alert("Muito bom! Continue estudando e você terá muito sucesso.");
-}else{
-    alert("Ahh que pena... Já tentou aprender outras linguagens?");
-}
+
+
+
+
+
+//const resposta = prompt(`Você gosta de estudar ${linguagemDeProgramacao}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+// if(resposta === '1'){
+//     alert("Muito bom! Continue estudando e você terá muito sucesso.");
+// }else{
+//     alert("Ahh que pena... Já tentou aprender outras linguagens?");
+// }
 
